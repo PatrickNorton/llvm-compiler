22b619936ede8601a7c698bdf20c8046adc9be9c
Patrick Norton <patrick.147.norton@gmail.com>
2022-03-27 08:50:45 -0700

    Added OptionTypeObject map_fn implementations

--- src/converter/type_obj/option.rs
+++ src/converter/type_obj/option.rs
@@ -2,10 +2,16 @@ use std::borrow::Cow;
 use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
-use crate::converter::builtins::NULL_TYPE;
+use once_cell::sync::OnceCell;
+
+use crate::converter::argument::ArgumentInfo;
+use crate::converter::builtins::{NULL_TYPE, OBJECT};
 use crate::converter::bytecode::Bytecode;
 use crate::converter::bytecode_list::BytecodeList;
 use crate::converter::fn_info::FunctionInfo;
+use crate::converter::generic::GenericInfo;
+use crate::converter::type_obj::TemplateParam;
+use crate::parser::line_info::LineInfo;
 
 use super::macros::{arc_eq_hash, try_from_type_obj, type_obj_from};
 use super::TypeObject;
@@ -19,8 +25,8 @@ pub struct OptionTypeObject {
 struct InnerOption {
     typedef_name: Option<String>,
     option_val: TypeObject,
-    map_fn: FunctionInfo,
-    flat_map_fn: FunctionInfo,
+    map_fn: OnceCell<FunctionInfo>,
+    flat_map_fn: OnceCell<FunctionInfo>,
 }
 
 impl OptionTypeObject {
@@ -28,8 +34,8 @@ impl OptionTypeObject {
         Self {
             value: Arc::new(InnerOption {
                 typedef_name: None,
-                map_fn: Self::get_map(option_val.clone()),
-                flat_map_fn: Self::get_flat_map(option_val.clone()),
+                map_fn: OnceCell::new(),
+                flat_map_fn: OnceCell::new(),
                 option_val,
             }),
         }
@@ -70,6 +76,7 @@ impl OptionTypeObject {
     }
 
     pub fn needs_make_option(maybe_option: &TypeObject, other: &TypeObject) -> bool {
+        // FIXME: Passing `object` into `other` does not work properly
         if let TypeObject::Option(option) = maybe_option {
             // TODO: Refer to Builtins
             NULL_TYPE.is_superclass(other) || option.get_option_val().is_superclass(other)
@@ -90,14 +97,35 @@ impl OptionTypeObject {
         other.is_subclass(&self.clone().into())
     }
 
+    pub fn is_subclass(&self, other: &TypeObject) -> bool {
+        match other {
+            TypeObject::Option(o) => {
+                self == o || o.get_option_val().is_superclass(self.get_option_val())
+            }
+            _ => false,
+        }
+    }
+
     pub fn generify_with(&self, parent: &TypeObject, args: Vec<TypeObject>) -> TypeObject {
         TypeObject::optional(self.value.option_val.generify_with(parent, args))
     }
 
     pub fn attr_type(&self, value: &str) -> Option<TypeObject> {
         match value {
-            "map" => Some(self.value.map_fn.clone().into()),
-            "flatMap" => Some(self.value.flat_map_fn.clone().into()),
+            "map" => Some(
+                self.value
+                    .map_fn
+                    .get_or_init(|| Self::get_map(self.get_option_val().clone()))
+                    .clone()
+                    .into(),
+            ),
+            "flatMap" => Some(
+                self.value
+                    .flat_map_fn
+                    .get_or_init(|| Self::get_flat_map(self.get_option_val().clone()))
+                    .clone()
+                    .into(),
+            ),
             _ => None,
         }
     }
@@ -138,11 +166,38 @@ impl OptionTypeObject {
     }
 
     fn get_map(ty: TypeObject) -> FunctionInfo {
-        todo!()
+        let param = TemplateParam::new("T".into(), 0, OBJECT.into());
+        let inner_fn =
+            FunctionInfo::with_args(ArgumentInfo::of_types([ty]), vec![param.clone().into()])
+                .to_callable();
+        let fn_info = FunctionInfo::new(
+            LineInfo::empty(),
+            "map".to_string(),
+            false,
+            GenericInfo::new(vec![param.clone()]),
+            ArgumentInfo::of_types([inner_fn]),
+            vec![TypeObject::optional(param.into())],
+        );
+        fn_info.set_generic_parent();
+        fn_info
     }
 
     fn get_flat_map(ty: TypeObject) -> FunctionInfo {
-        todo!()
+        let param = TemplateParam::new("T".into(), 0, OBJECT.into());
+        let option_param = TypeObject::optional(param.clone().into());
+        let inner_fn =
+            FunctionInfo::with_args(ArgumentInfo::of_types([ty]), vec![option_param.clone()])
+                .to_callable();
+        let fn_info = FunctionInfo::new(
+            LineInfo::empty(),
+            "flat_map".to_string(),
+            false,
+            GenericInfo::new(vec![param]),
+            ArgumentInfo::of_types([inner_fn]),
+            vec![TypeObject::optional(option_param)],
+        );
+        fn_info.set_generic_parent();
+        fn_info
     }
 }
 
