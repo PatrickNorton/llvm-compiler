8662495b794d27250e2dfd9056839dee822d8a84
Patrick Norton <patrick.147.norton@gmail.com>
2022-03-31 21:30:34 -0700

    Added documentation to PermissionLevel

--- src/converter/permission.rs
+++ src/converter/permission.rs
@@ -1,5 +1,13 @@
 use std::fmt::Display;
 
+/// Permission levels for the compiled code.
+///
+/// Levels:
+/// * [`Normal`](PermissionLevel::Normal): All user-written code
+/// * [`Stdlib`](PermissionLevel::Stdlib): Code in the stdlib; has access to
+/// `$native(sys)`
+/// * [`Builtin`](PermissionLevel::Builtin): Only for `__builtins__.newlang`;
+/// can use `$builtin`.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum PermissionLevel {
     Normal,
@@ -8,10 +16,26 @@ pub enum PermissionLevel {
 }
 
 impl PermissionLevel {
+    /// If the permission level is part of the standard library.
+    ///
+    /// Note that this is `true` for both [`PermissionLevel::Stdlib`] and
+    /// [`PermissionLevel::Builtin`], as the builtin file is also part of the
+    /// standard library.
+    ///
+    /// # Examples
+    /// assert!(!PermissionLevel::Normal.is_stdlib());
+    /// assert!(PermissionLevel::Stdlib.is_stdlib());
+    /// assert!(PermissionLevel::Builtin.is_stdlib());
     pub fn is_stdlib(self) -> bool {
         matches!(self, PermissionLevel::Stdlib | PermissionLevel::Builtin)
     }
 
+    /// If the permission level is the level used for builtin files.
+    ///
+    /// # Examples
+    /// assert!(!PermissionLevel::Normal.is_builtin());
+    /// assert!(!PermissionLevel::Stdlib.is_builtin());
+    /// assert!(PermissionLevel::Builtin.is_builtin());
     pub fn is_builtin(self) -> bool {
         matches!(self, PermissionLevel::Builtin)
     }
