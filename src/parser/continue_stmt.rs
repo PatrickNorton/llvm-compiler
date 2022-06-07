09c242be9adec29b40daf676c3cb0a0aa42a2841
Patrick Norton <patrick.147.norton@gmail.com>
2022-06-06 20:48:38 -0600

    Added docs to continue_stmt.rs

--- src/parser/continue_stmt.rs
+++ src/parser/continue_stmt.rs
@@ -5,6 +5,12 @@ use crate::parser::test_node::TestNode;
 use crate::parser::token::TokenType;
 use crate::parser::token_list::TokenList;
 
+/// The node representing a `continue` statement.
+///
+/// # Syntax
+/// ```
+/// "continue" ["if" TestNode]
+/// ```
 #[derive(Debug)]
 pub struct ContinueStatementNode {
     line_info: LineInfo,
@@ -12,14 +18,21 @@ pub struct ContinueStatementNode {
 }
 
 impl ContinueStatementNode {
+    /// Creates a new [`ContinueStatementNode`].
     pub fn new(line_info: LineInfo, cond: TestNode) -> Self {
         Self { line_info, cond }
     }
 
+    /// The conditional associated with the node, or [`TestNode::empty`] if no
+    /// conditional is attached.
     pub fn get_cond(&self) -> &TestNode {
         &self.cond
     }
 
+    /// Parses a [`ContinueStatementNode`] from the given list of tokens.
+    ///
+    /// If the first token in the list is not the `continue` keyword, this
+    /// method will panic.
     pub fn parse(tokens: &mut TokenList) -> ParseResult<ContinueStatementNode> {
         let (info, tok) = tokens.next_token()?.deconstruct();
         assert!(matches!(tok, TokenType::Keyword(Keyword::Continue)));
