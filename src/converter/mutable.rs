7a4674041a9d40534432d9fd0c9b61dec1598b45
Patrick Norton <patrick.147.norton@gmail.com>
2022-07-10 07:54:03 -0600

    Changed MutableNode::from_descriptor to not panic

--- src/converter/mutable.rs
+++ src/converter/mutable.rs
@@ -11,42 +11,42 @@ pub enum MutableType {
     MutMethod,
 }
 
-const DESCRIPTOR_PAIRS: &[(DescriptorNode, MutableType)] = &[
-    (DescriptorNode::Mut, MutableType::Mut),
-    (DescriptorNode::Final, MutableType::Final),
-    (DescriptorNode::Mref, MutableType::Mref),
-];
-
 impl MutableType {
     pub fn is_const_type(self) -> bool {
-        matches!(self, Self::Mref | Self::Standard)
+        matches!(self, Self::Mref | Self::Standard | Self::MutMethod)
     }
 
     pub fn is_const_ref(self) -> bool {
-        matches!(self, Self::Standard | Self::Final)
+        matches!(self, Self::Standard | Self::Final | Self::MutMethod)
     }
 
-    pub fn from_descriptor(descriptor: DescriptorNode) -> Self {
+    pub fn from_descriptor(descriptor: DescriptorNode) -> Option<Self> {
         match descriptor {
-            DescriptorNode::Mut => Self::Mut,
-            DescriptorNode::Final => Self::Final,
-            DescriptorNode::Mref => Self::Mref,
-            _ => panic!(
-                "Descriptor does not correspond to a mutability: {}",
-                descriptor.name()
-            ),
+            DescriptorNode::Mut => Some(Self::Mut),
+            DescriptorNode::Final => Some(Self::Final),
+            DescriptorNode::Mref => Some(Self::Mref),
+            _ => None,
         }
     }
 
     pub fn from_descriptors(descriptors: &HashSet<DescriptorNode>) -> Self {
-        DESCRIPTOR_PAIRS
+        descriptors
             .iter()
-            .find(|x| descriptors.contains(&x.0))
-            .map(|x| x.1)
-            .unwrap_or(MutableType::Standard)
+            .find_map(|&x| Self::from_descriptor(x))
+            .unwrap_or(Self::Standard)
     }
 
     pub fn from_nullable(descriptor: Option<DescriptorNode>) -> Self {
-        descriptor.map_or_else(|| Self::Standard, Self::from_descriptor)
+        descriptor.map_or_else(
+            || MutableType::Standard,
+            |x| {
+                Self::from_descriptor(x).unwrap_or_else(|| {
+                    panic!(
+                        "Descriptor does not correspond to a mutability: {}",
+                        x.name()
+                    )
+                })
+            },
+        )
     }
 }
