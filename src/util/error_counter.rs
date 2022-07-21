541138f01ed07dabd9a72bb99db66c07da17565b
Patrick Norton <patrick.147.norton@gmail.com>
2022-07-20 21:07:45 -0600

    Added tests to error_counter.rs

--- src/util/error_counter.rs
+++ src/util/error_counter.rs
@@ -36,3 +36,105 @@ impl Default for ErrorCounter {
         Self::new()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{sync::Arc, thread};
+
+    use super::ErrorCounter;
+
+    #[test]
+    fn empty() {
+        let counter = ErrorCounter::new();
+        assert_eq!(counter.get_warnings(), 0);
+        assert_eq!(counter.get_errors(), 0);
+    }
+
+    #[test]
+    fn single_thread_warnings() {
+        let counter = ErrorCounter::new();
+        for i in 0..10 {
+            assert_eq!(counter.get_warnings(), i);
+            counter.add_warning();
+        }
+    }
+
+    #[test]
+    fn single_thread_errors() {
+        let counter = ErrorCounter::new();
+        for i in 0..10 {
+            assert_eq!(counter.get_errors(), i);
+            counter.add_error();
+        }
+    }
+
+    #[test]
+    fn single_thread_interleaved() {
+        let counter = ErrorCounter::new();
+        for i in 0..10 {
+            assert_eq!(counter.get_warnings(), i);
+            assert_eq!(counter.get_errors(), i);
+            counter.add_warning();
+            counter.add_error();
+        }
+    }
+
+    #[test]
+    fn multi_thread_warnings() {
+        let counter = Arc::new(ErrorCounter::new());
+        let mut threads = Vec::with_capacity(10);
+        for _ in 0..10 {
+            let count = counter.clone();
+            let handle = thread::spawn(move || {
+                for _ in 0..10 {
+                    count.add_warning();
+                }
+            });
+            threads.push(handle);
+        }
+        for thread in threads {
+            thread.join().unwrap();
+        }
+        assert_eq!(counter.get_warnings(), 100);
+    }
+
+    #[test]
+    fn multi_thread_errors() {
+        let counter = Arc::new(ErrorCounter::new());
+        let mut threads = Vec::with_capacity(10);
+        for _ in 0..10 {
+            let count = counter.clone();
+            let handle = thread::spawn(move || {
+                for _ in 0..10 {
+                    count.add_error();
+                }
+            });
+            threads.push(handle);
+        }
+        for thread in threads {
+            thread.join().unwrap();
+        }
+        assert_eq!(counter.get_errors(), 100);
+    }
+
+    #[test]
+    fn multi_thread_interleaved() {
+        let counter = Arc::new(ErrorCounter::new());
+        let mut threads = Vec::with_capacity(10);
+        for _ in 0..10 {
+            let count = counter.clone();
+            let handle = thread::spawn(move || {
+                for _ in 0..10 {
+                    count.add_warning();
+                    count.add_error();
+                }
+            });
+            threads.push(handle);
+        }
+        for thread in threads {
+            thread.join().unwrap();
+        }
+        assert_eq!(counter.get_warnings(), 100);
+        assert_eq!(counter.get_errors(), 100);
+    }
+}
