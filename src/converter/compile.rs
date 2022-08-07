6523b695ed10f26f6a18c8474a2a926dd0ba9e0e
Patrick Norton <patrick.147.norton@gmail.com>
2022-08-07 09:52:18 -0600

    Fixed typedef support

--- src/converter/compile.rs
+++ src/converter/compile.rs
@@ -9,16 +9,19 @@ use itertools::Itertools;
 use crate::converter::builtins::ParsedBuiltins;
 use crate::macros::hash_map;
 use crate::parser::base::IndependentNode;
-use crate::parser::line_info::LineInfo;
+use crate::parser::line_info::{LineInfo, Lined};
 use crate::parser::parse::TopNode;
+use crate::parser::type_node::TypeNode;
+use crate::parser::typedef::TypedefStatementNode;
 
 use super::compiler_info::CompilerInfo;
 use super::convertible::BaseConvertible;
 use super::default_holder::DefaultHolder;
-use super::error::CompilerInternalError;
+use super::error::{CompilerException, CompilerInternalError, CompilerTodoError};
 use super::file_types::FileTypes;
 use super::global_info::GlobalCompilerInfo;
 use super::import_handler::{builtins_file, ImportHandler};
+use super::lang_obj::LangObject;
 use super::permission::PermissionLevel;
 use super::type_obj::{InterfaceType, TypeObject};
 use super::{builtins, CompileResult};
@@ -81,18 +84,31 @@ pub fn compile_all(
             .map(|(x, _)| x.clone())
             .collect(),
     );
+    // TODO? See if it's possible to calculate typedefs in an earlier pass
+    let addl_typedefs = all_files
+        .iter()
+        .map(|(file, (node, _))| {
+            let typedefs = determine_typedefs(node, global_info, file, &all_files)?;
+            Ok((file.clone(), typedefs))
+        })
+        .filter(|x| x.as_ref().map_or(true, |(_, x)| !x.is_empty()))
+        .collect::<CompileResult<HashMap<_, _>>>()?;
+    for (path, typedefs) in addl_typedefs {
+        let (_, types) = all_files.get_mut(&path).unwrap();
+        types.types.extend(typedefs)
+    }
     // NOTE: feature(iterator_try_collect) (#94047) would improve this
     let import_handlers = all_files
         .iter()
         .map(|(file, (_, types))| {
             let handler = ImportHandler::from_file_types(types)?;
-            Ok((file.clone(), handler))
+            Ok((file, handler))
         })
         .collect::<CompileResult<HashMap<_, _>>>()?;
     let mut all_infos = import_handlers
         .into_iter()
         .map(|(file, handler)| {
-            let predeclared = predeclared_types(&file, &all_files);
+            let predeclared = predeclared_types(file, &all_files);
             let info = CompilerInfo::with_handler(global_info, handler, predeclared)?;
             Ok((file, info))
         })
@@ -114,17 +130,17 @@ pub fn compile_all(
             Ok((file, defaults))
         })
         .collect::<CompileResult<HashMap<_, _>>>()?;
-    for (file, info) in &mut all_infos {
+    for (&file, info) in &mut all_infos {
         let mut defaults = default_holders.remove(file).unwrap();
         info.link(&all_files[file].0, &mut defaults)?;
         defaults.compile(info)?;
     }
     let export_infos = all_infos
         .iter()
-        .map(|(path, info)| (path.clone(), info.import_handler().export_info()))
+        .map(|(&path, info)| (path.clone(), info.import_handler().export_info()))
         .collect::<HashMap<_, _>>();
     global_info.set_export_infos(export_infos);
-    for (file, info) in &mut all_infos {
+    for (&file, info) in &mut all_infos {
         info.compile(&all_files[file].0)?
     }
     Ok(())
@@ -227,6 +243,83 @@ fn imported_ty<'a>(
     } else if let Option::Some(Either::Right(file)) = import_file.exports.get(name) {
         imported_ty(&all_files[file].1, all_files, name)
     } else {
+        for path in &import_file.wildcard_exports {
+            if let Option::Some(pair) = imported_ty(&all_files[path].1, all_files, name) {
+                return Some(pair);
+            }
+        }
         None
     }
+}
+
+fn determine_typedefs(
+    node: &TopNode,
+    global_info: &GlobalCompilerInfo,
+    file: &Path,
+    all_files: &HashMap<PathBuf, (TopNode, FileTypes)>,
+) -> CompileResult<HashMap<String, (TypeObject, LineInfo)>> {
+    let mut typedefs = HashMap::new();
+    for stmt in node {
+        if let IndependentNode::Typedef(typedef) = stmt {
+            let ty = parse_typedef(typedef, global_info, file, all_files)?;
+            let name = typedef.get_name().str_name().to_string();
+            typedefs.insert(name, (ty, typedef.line_info().clone()));
+        }
+    }
+    Ok(typedefs)
+}
+
+// FIXME/TODO: Merge with other type-deduction functions
+fn parse_typedef(
+    node: &TypedefStatementNode,
+    global_info: &GlobalCompilerInfo,
+    path: &Path,
+    all_files: &HashMap<PathBuf, (TopNode, FileTypes)>,
+) -> CompileResult<TypeObject> {
+    node_to_ty(node.get_type(), global_info, path, all_files)
+}
+
+fn node_to_ty(
+    node: &TypeNode,
+    global_info: &GlobalCompilerInfo,
+    path: &Path,
+    all_files: &HashMap<PathBuf, (TopNode, FileTypes)>,
+) -> CompileResult<TypeObject> {
+    if !node.get_name().get_post_dots().is_empty() {
+        return Err(CompilerTodoError::of("Conversion of typedefs to dotted types", node).into());
+    }
+    let ty_name = node.str_name();
+    let base = name_to_ty(node, ty_name, global_info, path, all_files)?;
+    if !node.get_subtypes().is_empty() {
+        let subtypes = node
+            .get_subtypes()
+            .iter()
+            .map(|x| node_to_ty(x, global_info, path, all_files))
+            .collect::<CompileResult<Vec<_>>>()?;
+        base.generify(node, subtypes)
+    } else {
+        Ok(base)
+    }
+}
+
+fn name_to_ty(
+    node: impl Lined,
+    ty_name: &str,
+    global_info: &GlobalCompilerInfo,
+    path: &Path,
+    all_files: &HashMap<PathBuf, (TopNode, FileTypes)>,
+) -> CompileResult<TypeObject> {
+    let (_, types) = &all_files[path];
+    if let Option::Some((ty, _)) = types.types.get(ty_name) {
+        return Ok(ty.clone());
+    }
+    // TODO: Check from-imports that (may) refer to other types
+
+    let builtins = global_info
+        .global_builtins()
+        .ok_or_else(|| CompilerInternalError::of("Builtins should be set by now", &node))?;
+    if let Option::Some(LangObject::Type(ty)) = builtins.get_name(ty_name) {
+        return Ok(ty.clone());
+    }
+    Err(CompilerException::of(format!("Could not find type '{}'", ty_name), node).into())
 }
